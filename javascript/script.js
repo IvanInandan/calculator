03131279f91cf745d03ccbb81b9d45ca73b7c234
IvanInandan <c.ivaninandan@gmail.com>
2024-06-18 13:38:31 -0700

    Add functionality for buttons to populate calculator screen

--- javascript/script.js
+++ javascript/script.js
@@ -1,7 +1,32 @@
 const btn = document.querySelectorAll("button");
+const input = document.querySelector(".input");
+const memory = document.querySelector(".memory");
 
 btn.forEach((button) => {
     button.addEventListener("click", (e) => {
-        console.log(`${button.textContent} has been clicked!`)
+        let val = button.getAttribute("class");
+
+        if (val.includes("delete")) {
+        }
+
+        else if (val.includes("clear")) {
+        }
+
+        else if (val.includes("equal")) {
+        }
+
+        else if (val.includes("operator")) {
+            memory.textContent += (input.textContent  + " " + button.textContent);
+        }
+
+        else if (val.includes("number")) {
+            input.textContent += button.textContent;
+        }
+
+        else if (val.includes("point")) {
+            if (!(input.textContent).includes(".")) {
+                input.textContent += button.textContent;
+            }
+        }
     })
 })
