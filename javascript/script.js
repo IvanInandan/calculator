3cf25d157fcf77ebdca4b0c6e1ffb5c62fc4a47e
IvanInandan <c.ivaninandan@gmail.com>
2024-06-18 14:03:54 -0700

    Add functionality to all buttons

--- javascript/script.js
+++ javascript/script.js
@@ -1,31 +1,68 @@
 const btn = document.querySelectorAll("button");
 const input = document.querySelector(".input");
 const memory = document.querySelector(".memory");
+let firstVal = undefined;
+let secondVal = undefined;
+let operator = undefined;
+let operatorFlag = false;
+
+function calculate(valOne, valTwo, operand) {
+    return 1;
+}
 
 btn.forEach((button) => {
     button.addEventListener("click", (e) => {
         let val = button.getAttribute("class");
 
         if (val.includes("delete")) {
+            if(input.textContent) {
+            input.innerText = input.innerText.slice(0, -1);
+            }
+
+            else alert("Input is already empty!");
         }
 
         else if (val.includes("clear")) {
+            firstVal = undefined;
+            secondVal = undefined;
+            operator = undefined;
+            operatorFlag = false;
+            memory.textContent = "";
+            input.textContent = "";
         }
 
         else if (val.includes("equal")) {
+            secondVal = Number(input.textContent);
+            memory.textContent += (" " + input.textContent);
+            input.textContent = calculate(firstVal, secondVal, operator);
         }
 
         else if (val.includes("operator")) {
+            firstVal = Number(input.textContent);
+            operator = button.textContent;
             memory.textContent += (input.textContent  + " " + button.textContent);
+            operatorFlag = true;
         }
 
         else if (val.includes("number")) {
-            input.textContent += button.textContent;
+            if (operatorFlag == false) {
+                input.textContent += button.textContent;
+            }
+
+            else {
+                input.textContent = button.textContent;
+                operatorFlag = false;
+            }
         }
 
         else if (val.includes("point")) {
-            if (!(input.textContent).includes(".")) {
+            if (operatorFlag == false) {
                 input.textContent += button.textContent;
+            }
+
+            else {
+                input.textContent = button.textContent;
+                operatorFlag = false;
             }
         }
     })
